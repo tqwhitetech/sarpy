869fbb1923f92ceea449bfa2df5f22dc9ebce4c4
Jason Casey <j.casey@beamio.net>
2020-04-20 17:59:48 -0400

    starting to get a treeview set up

---
+++ sarpy_gui_apps/apps/annotation_tool/panels/treeview_panel/treeview_panel.py
@@ -0,0 +1,41 @@
+try:
+    import Tkinter
+    import ttk
+except ImportError:  # Python 3
+    import tkinter as Tkinter
+    import tkinter.ttk as ttk
+
+from tkinter_gui_builder.panel_templates.widget_panel.widget_panel import AbstractWidgetPanel
+from tkinter_gui_builder.widgets.basic_widgets import Treeview
+
+
+class TreeviewPanel(AbstractWidgetPanel):
+    def __init__(self, parent):
+        '''
+        Constructor
+        '''
+        AbstractWidgetPanel.__init__(self, parent)
+        self.tree = Treeview(parent)
+
+        # Set the treeview
+        self.tree.config(columns=("first", "second"))  # this creates to seperate headings for treeview
+        self.tree.heading('#0', text='Item')
+        self.tree.heading('#1', text='Dose')
+        self.tree.heading('#2', text='Modification Date')
+        self.tree.column('#1', stretch=Tkinter.YES)
+        self.tree.column('#2', stretch=Tkinter.YES)
+        self.tree.column('#0', stretch=Tkinter.YES)
+        # Initialize the counter
+        self.i = 0
+        self.tree.pack()
+        self.pack()
+
+    def insert_data(self):
+        """
+        Insertion method.
+        """
+        self.tree.insert('', 'end', text="Item_"+str(self.i),
+                             values=(self.dose_entry.get() + " mg",
+                                     self.modified_entry.get()))
+        # Increment counter
+        self.i = self.i + 1
---
+++ tkinter_gui_builder/widgets/widget_wrappers/treeview.py
@@ -0,0 +1,13 @@
+try:
+    import Tkinter
+    import ttk
+except ImportError:  # Python 3
+    import tkinter as Tkinter
+    import tkinter.ttk as ttk
+
+from tkinter_gui_builder.widgets.widget_utils.widget_events import WidgetEvents
+
+
+class Treeview(ttk.Treeview, WidgetEvents):
+    def __init__(self, master=None, cnf=None, **kw):
+        ttk.Treeview.__init__(self, master=master, cnf=cnf, **kw)
